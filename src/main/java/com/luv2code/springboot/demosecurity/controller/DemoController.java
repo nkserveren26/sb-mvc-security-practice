1912843ababa053d8708a53e8f216cb3f72f1a94
nkserveren26 <norinoritoshi@outlook.jp>
2024-05-02 22:59:21 +0900

    leaderページを表示するようにControllerを修正

--- src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -11,4 +11,11 @@ public class DemoController {
 
         return "home";
     }
+
+    // add a request mapping for /leaders
+    @GetMapping("/leaders")
+    public String showLeaders() {
+
+        return "leaders";
+    }
 }
